1b6519145b9f6484237fc8ba4541058b7165e221
Y. Violet Guo <yueguo598@gmail.com>
2018-02-26 13:16:04 -0500

    Update a2-vg_logistic.py

--- A2/a2-vg_logistic.py
+++ A2/a2-vg_logistic.py
@@ -15,5 +15,25 @@ output param:
 '''
 
 #initializxe using placeholders
+#copied from linear
+#declare using a placeholder, feed in _data and _target to x ,y 
+x_dim, dum1 =_data.get_shape().as_list()
+x = tf.placeholder(tf.float32, shape=[x_dim, 784], name='dataX')
+# W initialize to a gaussian distr, honestly anything would work
+W = tf.Variable(tf.truncated_normal(shape=[784, 1], stddev=0.1), name='weights')
+W = tf.cast(W, dtype=tf.float32)
+b = tf.Variable(0.0, name='biases')
+y = tf.placeholder(tf.float32, shape=[x_dim, 1], name='targetY')
 
+
+#compute the current y_hat
+y_hat =  tf.matmul(x, W) + b
+regTerm = tf.multiply(_regLambda, tf.reduce_mean(tf.square(W)))
+
+#compute crossEntropyError
+crossEntropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=y, logits= y_hat)
+crossEntropyError = tf.reduce_mean(crossEntropy)/2.0 + regTerm/2.0
+
+optimizer = tf.train.AdamOptimizer(learning_rate = _learningRate)
+train = optimizer.minimize(loss=crossEntropyError)
 
