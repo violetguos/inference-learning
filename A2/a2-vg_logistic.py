a52eb649eee41282f8bc8c4f9e2bcd9497ea6284
Y. Violet Guo <yueguo598@gmail.com>
2018-02-26 13:06:08 -0500

    Create a2-vg_logistic.py

---
+++ A2/a2-vg_logistic.py
@@ -0,0 +1,19 @@
+'''
+function to build a logistic graph for training
+
+input param:
+  _data: x
+  _target: y
+  _learningRate: learning_rate
+  _regLambda: regularizer term
+
+output param:
+  W
+  x
+  y
+  train, optimizer
+'''
+
+#initializxe using placeholders
+
+
