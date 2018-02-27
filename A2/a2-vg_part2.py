da3ed1a04fcbccdd75a2777f7c5e2084f568f1fd
Y. Violet Guo <yueguo598@gmail.com>
2018-02-27 10:22:07 -0500

    Update a2-vg_part2.py

--- A2/a2-vg_part2.py
+++ A2/a2-vg_part2.py
@@ -6,5 +6,6 @@ one-hot encode faces and notMNIST m = np.eye(num)[targetLabel]
 W = 784 by 10 or 1024 by 6
 optimizer funtion = adamOptimizer from tf
 
+error = tf.nn.softmax_cross_entropy_with_logits
 
 """
