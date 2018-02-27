c20ccab7cf07ae9372795497912da57ad9326cb4
Y. Violet Guo <yueguo598@gmail.com>
2018-02-27 10:13:28 -0500

    Update a2-vg_part2.py

--- A2/a2-vg_part2.py
+++ A2/a2-vg_part2.py
@@ -1,3 +1,10 @@
 """
 Implement multi class classification
+
+initialize
+one-hot encode faces and notMNIST m = np.eye(num)[targetLabel]
+W = 784 by 10 or 1024 by 6
+optimizer funtion = adamOptimizer from tf
+
+
 """
