68032e5946d4512ee866a1226d9e601bd44e9bed
proguorammer <yueguo598@gmail.com>
2018-03-19 20:42:44 -0400

    start pseudo code

---
+++ A3/vg-branch/pseudo.py
@@ -0,0 +1,40 @@
+'''
+	this file contains pseudo code for A3
+'''
+
+# part 1 
+class NeuralNetowork:
+
+	def __init__():
+		self.num_layer
+		self.hidden_unit
+
+
+#part 1.1 layer wise building bock
+def build_layer(input_tensor, num_units):
+	input_tensor # dim []
+
+	'''
+	input tensor
+	[X_1  X_2 ]
+	
+	xavier
+	[ W_1,1 
+	  W_1,2 ]
+	'''
+
+	W = Xavier_init(num_input = X dim, output = 1 )
+	tf.mult(input_tensor, W) #vectorized mulitplication
+	
+	
+#part 1.2 build a NN and try learning rates
+learningRateArr = [0.001, 0.01, 0.005]
+
+def buildNetwork():
+	# TODO: initialize nodes, connect weight edges
+	# Relu Layer
+
+
+
+	
+
